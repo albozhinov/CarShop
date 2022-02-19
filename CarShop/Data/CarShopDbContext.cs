f1f551ffc7bc2a0f7b3bb25f58893d202e1a480d
albozhinov <a.bozhinov90@gmail.com>
2022-02-19 17:52:48 +0200

    Add project files.

---
+++ CarShop/Data/CarShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace CarShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class CarShopDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=CarShop;Integrated Security=True;");
+            }
+        }
+    }
+}
---
+++ CarShop/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace CarShop
+{
+    using System.Threading.Tasks;
+    using CarShop.Data;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<CarShopDbContext>()
+                    .Add<IViewEngine, CompilationViewEngine>())
+                //.WithConfiguration<CarShopDbContext>(context => context
+                //    .Database.Migrate())
+                .Start();
+    }
+}
